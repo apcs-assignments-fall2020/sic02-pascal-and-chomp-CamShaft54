28633db0644486a2f71d9dfdb9cee7a8f9c5782f
camshaft54 <cam.shaw1234@gmail.com>
2021-01-20 08:34:13 -0800

    Complete Pascal Triangle Method

--- src/main/java/Pascal.java
+++ src/main/java/Pascal.java
@@ -7,7 +7,16 @@
 public class Pascal
 {
     public static int[][] pascalTriangle(int n) {
-        // YOUR CODE HERE
-        return null;
+        int[][] triangle = new int[n+1][n+1];
+        for (int i = 0; i < n+1; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    triangle[i][j] = 1;
+                } else {
+                    triangle[i][j] = triangle[i-1][j] + triangle[i-1][j-1];
+                }
+            }
+        }
+        return triangle;
     }
 }
